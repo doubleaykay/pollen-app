e34b619b6ab80238ad3d43fe413933ed6f24a12d
doubleaykay <the.a.cars@gmail.com>
2017-07-11 18:35:25 -0400

    start making menu

--- app/src/main/java/mystikos/pollen/MainActivity.java
+++ app/src/main/java/mystikos/pollen/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.TextView;
 
