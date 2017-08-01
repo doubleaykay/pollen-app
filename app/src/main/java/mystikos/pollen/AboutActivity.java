df760d05345e6dd9f75d481ab9412e80950e88fc
doubleaykay <the.a.cars@gmail.com>
2017-08-01 17:39:17 -0400

    finalize about screen

--- app/src/main/java/mystikos/pollen/AboutActivity.java
+++ app/src/main/java/mystikos/pollen/AboutActivity.java
@@ -1,8 +1,11 @@
 package mystikos.pollen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -14,5 +17,21 @@ public class AboutActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         setTitle("About");
+    }
+
+    public void github(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.github.com/doubleaykay"));
+        startActivity(intent);
+    }
+
+    public void xda(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.forum.xda-developers.com/member.php?u=5535848"));
+        startActivity(intent);
     }
 }
